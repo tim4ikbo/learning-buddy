5b9986136cd541b00658b180eb14e169ce54eac0
tim4ikbo <bobkov.star.tima@gmail.com>
2025-02-28 18:21:35 +0200

    window_useEffect

--- src/app/canvas/page.tsx
+++ src/app/canvas/page.tsx
@@ -1,58 +1,32 @@
 "use client";
+import { useState, useEffect } from 'react';
 import { Stage, Layer, Rect, Circle, Image } from 'react-konva';
 import useImage from 'use-image';
 
 const LionImage = () => {
     const [image] = useImage('https://konvajs.org/assets/lion.png');
     return <Image image={image} />;
-};
+  };
 
-
-
-
-import { useState, useEffect } from "react";
-
-const useInnerSize = () => {
-    const [size, setSize] = useState({
-        width: 0,
-        height: 0,
-    });
-
+  export default function Canvas() {
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+  
     useEffect(() => {
-        const handleResize = () => {
-            setSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        };
-
-        window.addEventListener("resize", handleResize);
-
-        // Cleanup event listener on unmount
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
     }, []);
-
-    return size;
-};
-
-
-
-export default function Canvas() {
-    const { width, height } = useInnerSize();
+  
     return (
-        // Stage - is a div wrapper
-        // Layer - is an actual 2d canvas element, so you can have several layers inside the stage
-        // Rect and Circle are not DOM elements. They are 2d shapes on canvas
-        <Stage width={width} height={height}>
-            <Layer>
-                <Rect width={50} height={50} fill="red" draggable />
-                <Circle x={200} y={200} stroke="black" radius={50} draggable />
-                <LionImage />
-            </Layer>
-        </Stage>
+      <Stage width={width} height={height}>
+        <Layer>
+          <Rect width={50} height={50} fill="red" draggable />
+          <Circle x={200} y={200} stroke="black" radius={50} draggable />
+          <LionImage />
+        </Layer>
+      </Stage>
     );
-}
+  }
+  
 
 
